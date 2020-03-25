4ec244f898dcbe42e0843b51ecb7515f9eb4d4d9
n3bizzz <n3bizzz@gmail.com>
2020-03-25 11:18:58 +0300

    js homework2

--- js/homework2.js
+++ js/homework2.js
@@ -1,4 +1,4 @@
-//Задача 1 найти максимальный эл-т
+//Задача 1
 let myArr=[[],[],[]];
 let max;
 let maxi;
@@ -16,3 +16,48 @@ for(let i=0; i<3; i++)
         if (myArr[i][j]>max ){max = myArr[i][j]; maxi=i; maxj=j; }
     }
 console.log('elem',max,'index',maxi,maxj);
+
+
+//Задача 2
+let str1=prompt('Введите исходную строку');
+let str2=prompt('Введите искомую подстроку');
+str1=str1.trim();
+str2=str2.trim();
+console.log(str1);
+console.log(str2);
+let a=str1.indexOf(str2);
+let b=0;
+if (a>=0) {
+    console.log('Найдено совпадение на позиции',a);
+    b=1;
+    while (a >= 0 && a+1<str1.length) {
+        a = str1.indexOf(str2, a + 1);
+        if (a >= 0) {
+            console.log('Найдено совпадение на позиции', a);
+            b++;
+        }
+    }
+    console.log('всего  совпадений',b);
+}
+else
+    console.log('Совпадений не найдено');
+
+//Задача 3
+str1=prompt('Введите строку');
+str1=str1.trim();
+let stArr=str1.split("");
+a = stArr.shift();
+str1=a.toUpperCase()+stArr.join('');
+console.log(str1);
+
+//Задача 4
+let anArr = ['dog','cat', 'rabbit','mouse'];
+//если знаем индекс 'cat'
+anArr.splice(1,1);
+console.log(anArr);
+//если не знаем индекс 'cat'
+for(let i=0; i<anArr.length; i++)
+{
+    if(anArr[i] === 'cat'){anArr.splice(i,1);}
+}
+console.log(anArr);
